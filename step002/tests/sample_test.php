0d68cddb01e9fb4bb67dc06f6ba02195f7641555
MAIKOKAWATA <maiko.kawata@petabit.co.jp>
2018-12-06 22:19:10 +0900

    testcode

---
+++ step002/tests/sample_test.php
@@ -0,0 +1,72 @@
+<?php
+
+include('./vendor/autoload.php');
+include('./step002/sample.php');
+
+use PHPUnit\Framework\TestCase;
+
+class JihanTest extends TestCase
+{
+    public function test000()
+    {
+        $res = [];
+        $this->assertInternalType('array', $res);
+        $this->assertInternalType('int', 1);
+    }
+    // inputValue()の検証 returnの型
+    public function test001()
+    {
+        $_SERVER['argv'][1] = 'コーラ';
+        $_SERVER['argv'][2] = '200';
+        $res = inputValue();
+        $this->assertInternalType('array', $res);
+    }
+    public function test002()
+    {
+        // $_SERVER['argv']に引数を設定する
+        $_SERVER['argv'][1] = 'コーラ';
+        $_SERVER['argv'][2] = '200';
+
+        $res = inputValue();
+
+        $this->assertEquals($res[0], 'コーラ');
+        $this->assertEquals($res[1], 200);
+
+        $this->assertInternalType('string', $res[0]);
+        $this->assertInternalType('int', $res[1]);
+    }
+
+    /*
+
+    // inputValue()の検証 returnの中身
+    public function test002()
+    {
+        // $_SERVER['argv']に引数を設定する
+        $_SERVER['argv'][1] = 'コーラ';
+        $_SERVER['argv'][2] = '200';
+
+        $res = inputValue();
+
+        $this->assertEquals($res[0], 'コーラ');
+        $this->assertEquals($res[1], 200);
+
+        //$this->assertInternalType('string', $res[0]);
+        //$this->assertInternalType('int', $res[1]);
+    }
+
+    // calc()の検証
+    public function test003()
+    {
+        $otsuri = calc('コーラ', 200);
+        //$this->assertEquals(0, $otsuri);
+
+        $otsuri = calc('カルピス', 200);
+        //$this->assertEquals(0, $otsuri);
+    }
+
+    // outpitValue()の検証
+    public function test004()
+    {
+
+    }*/
+}
