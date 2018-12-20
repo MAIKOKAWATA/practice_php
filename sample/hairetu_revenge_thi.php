799315592c3893cb52087e93eb20e5224456faf2
MAIKOKAWATA <maiko.kawata@petabit.co.jp>
2018-12-20 14:55:51 +0900

    配列あとちょっと

--- sample/hairetu_revenge_thi.php
+++ sample/hairetu_revenge_thi.php
@@ -31,125 +31,76 @@ $youtubers = [
             "origin_genesisone",
             ]
     ],
+/*    "3" => [
+        "name" =>"saguwa",
+        "age" => 20,
+        "belongs" => [
+            "origin_nakamaya",
+            "origin_genesisone",        
+        ]
+    ],*/
 ];
 /** ------------------------------------------------------
- * 1 .「全員の所属情報(belongs)のみをとってくる」をやってみる
+ * 1 .「全員の情報から、各人の所属情報(belongs)だけ取ってくる」をやってみる
  * -------------------------------------------------------
  */
-/*$belongs=[];
+/*
+$belongs=[];
  foreach($youtubers as $key=>$youtuber){
-    $belongs[] = $youtubers[$key]["belongs"];
-}*/
+    $belongs[] = $youtuber["belongs"];
+}
+*/
+//var_dump($belongs);exit;
 
 /** -------------------------------------------------------
- * 2. 「1人目の所属情報を全て保持する」をやってみる
+ * 2. 「1人目の所属情報を$same_belongsにいれる」をやってみる
  * --------------------------------------------------------
  */
-/*$same_belongs = "";
+/*
+$same_belongs = [];
 foreach($youtubers as $key=>$youtuber){
     if($key==0){
-        $same_belongs = $youtubers[$key]["belongs"];
+        $same_belongs = $youtuber["belongs"];
     }
-}*/
-
+}
+*/
+//var_dump($same_belongs);exit;
 
 /** -------------------------------------------------------
  * 3. 「2人目の所属情報を保持する」をやってみる
  *  -------------------------------------------------------
  */
-/*
-$same_belongs = "";
-$comparison_belongs = [];
-foreach($youtubers as $key=>$youtuber){
-    if($key==0){
-        $same_belongs = $youtubers[$key]["belongs"];
-        //var_dump($same_belongs);exit;
-    }else{
-        $comparison_belongs[] = $youtubers[$key]["belongs"];
-        //var_dump($comparison_belongs);exit;
-    }
-}
-*/
-/** -------------------------------------------------------
- * 4. 1人目の所属情報($same_belongs)と
- * 2人目の所属情報(compa_belongs)を比較し、
- * 共通項を保持する
- *  -------------------------------------------------------
- */
 
-$same_belongs = "";
-$comparison_belongs = [];
-$result = [];
-foreach($youtubers as $key=>$youtuber){
-    if($key==0){
-        $same_belongs = $youtubers[$key]["belongs"];
-        //var_dump($same_belongs);exit;
-    }else{
-        $comparison_belongs[] = $youtubers[$key]["belongs"];
-        //var_dump($comparison_belongs);exit;
-    }
-}
-
-foreach($same_belongs as $same_belong){
-    //var_dump($same_belong);exit;
-    foreach($comparison_belongs as $comparison_belong){
-        //var_dump($comparison_belong);exit;
-        if($same_belong == $comparison_belong){
-            $result[] = $same_belong;
-            var_dump($result);exit;
-        }
-    }
-}
-//var_dump($result);exit;
-
-
-
- /*
-$belongs = [];
 $same_belongs = [];
 $comparison_belongs = [];
 $result = [];
 foreach($youtubers as $key=>$youtuber){
     if($key==0){
-        $same_belongs[] = $youtubers[$key]["belongs"];
-//        var_dump($same_belongs);exit;
+        $same_belongs = $youtuber["belongs"];//1人目
     }else{
-        $comparison_belongs[] = $youtubers[$key]["belongs"];
-//        var_dump($comparison_belongs);exit;
+        $comparison_belongs = $youtuber["belongs"];
+        //var_dump($comparison_belongs);exit;
     }
+    //return $same_belongs;
+    //return $comparison_belongs;
 }
-//var_dump($comparison_belongs);exit;
+//$same_belongsと$comparison_belongsを
+//それぞれforeachでまわして値一個ずつにして比べる必要有
 foreach($same_belongs as $same_belong){
-    //var_dump($same_belong);exit;
     foreach($comparison_belongs as $comparison_belong){
-        //var_dump($comparison_belong);exit;
-        if($same_belong == $comparison_belong){
-            $result[] = $same_belong;
-        }
-    }
-}
-
-
-foreach($youtubers as $key=>$youtuber){
-    if($key==0){
-        $same_belongs[] = $youtubers[$key]["belongs"];
-//        var_dump($same_belongs);exit;
-    }else{
-        $comparison_belongs[] = $youtubers[$key]["belongs"];
-//        var_dump($comparison_belongs);exit;
-    }
-}
-//var_dump($same_belongs);exit;
-//var_dump($comparison_belongs);exit;
-foreach($same_belongs as $same_belong){
-    //var_dump($same_belong);exit;
-    foreach($comparison_belongs as $comparison_belong){
-        //var_dump($comparison_belong);exit;
         if($same_belong == $comparison_belong){
             $result[] = $comparison_belong;
         }
     }
 }
 
-var_dump($result);exit;
-*/
+//    if($same_belongs == $comparison_belongs){
+//        $result = $comparison_belongs;
+        //これでは配列同士で比べるので$resultがarray(0)になってしまう
+//    }
+var_dump($same_belongs);
+echo "\n-----------------------------\n";
+var_dump($comparison_belongs);
+echo "\n-----------------------------\n";
+var_dump($result);
+exit;
