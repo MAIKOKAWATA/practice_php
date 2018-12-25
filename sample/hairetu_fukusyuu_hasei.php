181a6dc049fee774a54f59c699b68a2bfd1bb8b7
MAIKOKAWATA <maiko.kawata@petabit.co.jp>
2018-12-25 18:26:18 +0900

    12/25

--- sample/hairetu_fukusyuu_hasei.php
+++ sample/hairetu_fukusyuu_hasei.php
@@ -56,16 +56,17 @@ $sanrio_characters = [
  * サンリオの配列データをつかって、一番使われている色が多いものを算出する
  * 1. 色の情報を取り出す
  * 2. 色の情報を一つの配列にぶっこむ
- * 2. 色の情報が出るたびにカウントする
- * 3. カウントした値の大きい色を返す
+ * 3. 使う色数がもっとも多いものを算出する
+ * 4. キャラ名を返す
  */
 
 $lineupColors = [];
-foreach ($sanrio_characters as $key => $sanrio_character) {//1.2.
+foreach ($sanrio_characters as $key => $sanrio_character) {
 //    $countColors[] = array_count_values($sanrio_character["colors"]);    
-    $countColors[] = count($sanrio_character["colors"]);    
-    $maxCount = max($countColors);
+    $countColors[] = count($sanrio_character["colors"]);    //1,2
+    $maxCount = max($countColors);                          //3
 }
+
 /*
 foreach ($countColors as $countcolor) {
     $maxCount = max($countColor);
@@ -76,7 +77,7 @@ var_dump($countColors);
 echo "\n-------------------------\n";
 var_dump($maxCount);
 echo "\n-------------------------\n";
-var_dump($key["3"]);
+//var_dump($key["3"]);
 exit;
 
 
