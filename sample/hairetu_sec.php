aefa91c5008d69eb0b9352e11dd56ff64e04e052
MAIKOKAWATA <maiko.kawata@petabit.co.jp>
2019-01-02 20:33:40 +0900

    復習

---
+++ sample/hairetu_sec.php
@@ -0,0 +1,42 @@
+<?php
+$sanrioCharactersMyFavorite = [
+	"0" => [
+		"name" => "pochacco",
+		"birthday" => "2/29",
+		"colors" => [
+			"black",
+			"white",
+			"red",
+		],
+	],
+	"1" => [
+		"name" => "pompompurin",
+		"birthday" => "4/16",
+		"colors" => [],
+	],
+	"2" => [
+		"name" => "",
+		"birthday" => "",
+		"colors" => [],
+	],
+	"3" => [
+		"name" => "",
+		"birthday" => "",
+		"colors" => [],
+	],
+	"4" => [
+		"name" => "",
+		"birthday" => "",
+		"colors" => [],
+	],
+	"5" => [
+		"name" => "",
+		"birthday" => "",
+		"colors" => [],
+	],
+	"6" => [
+		"name" => "",
+		"birthday" => "",
+		"colors" => [],
+	],
+];
